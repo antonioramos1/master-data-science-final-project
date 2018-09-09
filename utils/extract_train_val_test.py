efb5ef47d6618ec863389bd5ff488f7ee980a755
lorenzochavez <lorenzo.cht@gmail.com>
2018-09-10 00:44:29 +0200

    script updated

--- utils/extract_train_val_test.py
+++ utils/extract_train_val_test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import os
 import sys
@@ -5,55 +6,61 @@ import shutil
 from tqdm import tqdm
 
 def extract_train_val_test(category, ratio, sampling=None):
-    customer_df = pd.read_csv("./customer_df.csv") #modify paths accordingly
-    retrieval_df = pd.read_csv("./retrieval_df.csv")
+    customer_df = pd.read_csv("../notebooks/customer_df.csv") #modify paths accordingly
+    retrieval_df = pd.read_csv("../notebooks/retrieval_df.csv")
+    dataset_path = "../photos_resized" #modify paths accordingly
 
     avail_categories = customer_df["category"].unique()
     assert category in avail_categories, print("Please use a valid category: {}".format(" ".join(avail_categories)))
 
-    
     customer_df = customer_df[customer_df["category"] == category] #filter by category
     retrieval_df = retrieval_df[retrieval_df["category"] == category]
-
     customer_df = customer_df.drop_duplicates(subset=["photo"]) #removing same bounding boxes to avoid bias number of matches
 
     merged_df = customer_df.merge(retrieval_df, how="inner", on="id", suffixes=("_cust", "_retr")) #these are how many all:all matches are available
-    
-    n_ratio = int(len(merged_df)*ratio[0]) #how many photos in each test, rounding down to keep same number for both sets
-    
-    valid_df = merged_df.sample(n_ratio, random_state=2018) #randomly splits sets
-    dif_index = list((merged_df.index).difference(valid_df.index)) #difference in indices to take the remaining photos
-    test_df = merged_df.loc[dif_index[:n_ratio]] #indexing again on dif_index to ensure same number of photos in both sets
-    
+    unmatched_df = retrieval_df[~retrieval_df['id'].isin(merged_df['id'].unique())] #dataframe for all unmatched itesm
+
+    np.random.seed(2018) #select how many photos in each set, randomly shuffled
+    #matched items
+    perm = np.random.permutation(merged_df['photo_cust'].unique()) #permutes list of photos randomly
+    split = int(len(merged_df['photo_cust'].unique())*(ratio[0]))
+    valid, test = np.split(perm, [split])
+    valid_df = merged_df[merged_df['photo_cust'].isin(valid)]
+    test_df = merged_df[merged_df['photo_cust'].isin(test)]
+
+    #unmatched items
+    perm = np.random.permutation(unmatched_df['photo'])
+    split = int(len(unmatched_df['photo'])*ratio[0])
+    valid, test = np.split(perm, [split])
+    unmatched_valid = unmatched_df[unmatched_df['photo'].isin(valid)]
+    unmatched_test = unmatched_df[unmatched_df['photo'].isin(test)]
+
     if sampling != None:
-        valid_df = valid_df.sample(sampling, random_state=2018) #in case we want to further reduce the sets we take a sample
-        test_df = test_df.sample(sampling, random_state=2018)    
+        all_df = []
+        for df in [valid_df, test_df, unmatched_valid, unmatched_test]:
+            all_df.append(df.sample(sampling, random_state=2018)) #in case we want to further reduce the sets we take a sample)
+        valid_df, test_df, unmatched_valid, unmatched_test = all_df[0], all_df[1], all_df[2], all_df[3]
 
-    return valid_df, test_df
+    for step in zip([[valid_df, unmatched_valid] , [test_df, unmatched_test]], ["validation", "test"]):
+        photos_list_cust = step[0][0]["photo_cust"].unique().tolist()
+        photos_list_cust = [str(photo) + ".jpg" for photo in photos_list_cust]
 
-def move_files(df, set_name, category):
-    dataset_path = "./photos_resized" #modify paths accordingly
-    photos_list_cust = df["photo_cust"].unique().tolist()
-    photos_list_cust = [str(photo) + ".jpg" for photo in photos_list_cust]
-    
-    photos_list_retr = df["photo_retr"].unique().tolist()
-    photos_list_retr = [str(photo) + ".jpg" for photo in photos_list_retr]
+        photos_list_retr = step[0][0]["photo_retr"].unique().tolist() + step[0][1]["photo"].tolist()
+        photos_list_retr = [str(photo) + ".jpg" for photo in photos_list_retr]
 
-    output_path = os.path.join(".", "photos_classified", category, set_name)
-    if not os.path.exists(output_path):
-        os.makedirs(output_path, exist_ok=True)
-        os.makedirs(os.path.join(output_path, "customer"))
-        os.makedirs(os.path.join(output_path, "retrieval"))
-        
-    print("Creating customer " + set_name + " images")
-    for photo in tqdm(photos_list_cust):
-        shutil.copy(os.path.join(dataset_path, photo), os.path.join(output_path, "customer", photo))
-        
-    print("Creating retrieval " + set_name + " images")
-    for photo in tqdm(photos_list_retr):
-        shutil.copy(os.path.join(dataset_path, photo), os.path.join(output_path, "retrieval", photo))
+        output_path = os.path.join("..", "photos_classified", category, step[1])
+        if not os.path.exists(output_path):
+            os.makedirs(output_path, exist_ok=True)
+            os.makedirs(os.path.join(output_path, "customer"))
+            os.makedirs(os.path.join(output_path, "retrieval"))
+
+        print("Creating customer " + step[1] + " images")
+        for photo in tqdm(photos_list_cust):
+            shutil.copy(os.path.join(dataset_path, photo), os.path.join(output_path, "customer", photo))
+
+        print("Creating retrieval " + step[1] + " images")
+        for photo in tqdm(photos_list_retr):
+            shutil.copy(os.path.join(dataset_path, photo), os.path.join(output_path, "retrieval", photo))
 
 if __name__ == "__main__":
-    valid_df, test_df = extract_train_val_test(sys.argv[1], sys.argv[2], sys.argv[3])
-    move_files(valid_df, "validation", sys.argv[1])
-    move_files(test_df, "test", sys.argv[1])
+    extract_train_val_test(sys.argv[1], sys.argv[2], sys.argv[3]) #need to fix sampling=None parameter
